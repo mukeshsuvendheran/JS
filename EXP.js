825e47e6050bb300f46c8b3cc63cb61081062ad8
mukeshsuvendheran <109419653+mukeshsuvendheran@users.noreply.github.com>
2022-08-24 00:08:54 +0530

    Create EXP.js

---
+++ EXP.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS training</title>
+</head>
+<body>
+    <script>
+        alert("This website is harmfull,you want to sure!");
+        alert("plzz continue");
+        alert(4*5);
+
+        console.log("hii the system is hacked");
+
+        let name = " hii i m MUKESH"
+        alert(name);
+        let msg = "hii i m your console"
+        console.log(msg);
+    </script>
+</body>
+</html>
